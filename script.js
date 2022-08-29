a5f5f13bf2a573f7e35b416907c76936c6de2d2f
fajarajikusuma <fajikusuma@gmail.com>
2022-08-29 22:42:29 +0700

    add product section

--- script.js
+++ script.js
@@ -4,3 +4,20 @@ const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', () => {
     nav.classList.toggle('slide');
 });
+
+const productContainers = [...document.querySelectorAll('.product-container')];
+const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+const preBtn = [...document.querySelectorAll('.pre-btn')];
+
+productContainers.forEach((item, i) => {
+    let containerDimensions = item.getBoundingClientRect();
+    let containerWidth = containerDimensions.width;
+
+    nxtBtn[i].addEventListener('click', () => {
+        item.scrollLeft += containerWidth;
+    })
+
+    preBtn[i].addEventListener('click', () => {
+        item.scrollLeft -= containerWidth;
+    })
+})
